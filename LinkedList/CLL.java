833d302e202664af80e20bf48d9a9f45e627565b
vaibhav <saivaibhav1805@gmail.com>
2026-06-16 13:11:14 +0530

    Problems on single Linked List

---
+++ LinkedList/CLL.java
@@ -0,0 +1,75 @@
+package LinkedList;
+
+public class CLL {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public CLL(){
+        this.size=0;
+        this.head=null;
+        this.tail=null;
+    }
+    public void insert(int val){
+        Node node=new Node(val);
+        if(head==null){
+            head=node;
+            tail=node;
+            return;
+        }
+        tail.next=node;
+        node.next=head;
+        tail=node;
+    }
+
+    public void delete(int val){
+        Node node=head;
+        if(node==null){
+            return;
+        }
+        if(node.val==val){
+            head=head.next;
+            tail.next=head;
+            return;
+        }
+        do{
+            Node n=node.next;
+            if(n.val==val){
+                node.next=n.next;
+                break;
+            }
+            node=node.next;
+        }while(node!=head);
+    }
+
+    public void display(){
+        Node node=head;
+        if(head!=null){
+            do{
+                System.out.print(node.val+"->"+head.val);
+                node=node.next;
+            }while(node!=head);
+        }
+    }
+
+    private class Node{
+        private int val;
+        private Node next;
+
+        public Node(int val){
+            this.val=val;
+        }
+
+    }
+    public static void main(String[] args) {
+        CLL list=new CLL();
+        list.insert(6);
+        list.insert(3);
+        list.insert(2);
+        list.insert(1);
+        list.display();
+        list.delete(3);
+        list.display();
+        
+    }
+}
